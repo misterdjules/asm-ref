b697719c4c0eb3d3d5951955f1ee0fd448026b8d
Julien Gilli <jgilli@fastmail.fm>
2014-06-23 16:49:27 -0400

    Update x86 ref importer to actually parse Intel Software Manual instead of generating fake values.

--- tools/x86-ref-importer/importer.js
+++ tools/x86-ref-importer/importer.js
@@ -1,36 +1,106 @@
 var sqlite3           = require('sqlite3');
-var db                = new sqlite3.Database('x86-ref.sqlite');
+var db                = null;
 var intelX86RefParser = require('./intel-x86-ref-parser');
+var async			  = require('async');
+var child_process     = require('child_process');
 
-db.serialize(function() {
-	db.run('CREATE TABLE instructions( ' 	+
-		   'mnemonic TEXT PRIMARY KEY, ' 	+
-		   'opcode TEXT, ' 					+
-		   'synopsis TEXT, ' 				+
-		   'short_description TEXT, ' 		+
-		   'long_description TEXT, ' 		+
-		   'affected_flags TEXT)');
+var DROP_INSTRUCTIONS_TABLE = 'DROP TABLE IF EXISTS instructions;';
+var X86_REF_PDF_FILE_PATH   = './x86.pdf'
+var X86_REF_TXT_FILE_PATH   = './x86.txt'
 
-	var addInstructionStatement = db.prepare('INSERT INTO instructions VALUES (?, ?, ?, ?, ?, ?)');
+console.log('Importing...');
 
-	intelX86RefParser.parse('x86.txt', function(instructionObject) {
-		var mnemonic          = instructionObject.mnemonic;
-		var opcode            = instructionObject.opcode;
-		var synopsis		  = instructionObject.synopsis;
-		var short_description = instructionObject.short_description;
-		var long_description  = instructionObject.long_description;
-		var affected_flags    = instructionObject.affected_flags;
+async.waterfall([
+	function removeOldX86TxtRef(callback) {
+		var rm;
 
-		addInstructionStatement.run(mnemonic,
-									opcode,
-									synopsis,
-									short_description,
-									long_description,
-									affected_flags);
+		console.log('Removing old x86 text reference...');
+
+		rm = child_process.spawn('rm', ['-f', X86_REF_TXT_FILE_PATH]);
+		rm.on('close', function (exitCode) {
+			if (exitCode !== 0) {
+				return callback(exitCode);
+			} else {
+				return callback();
+			}
+		});
+	},
+	function x86RefPdfToText(callback) {
+		var pdfToText;
+
+		console.log('Converting current x86 PDF ref to txt for easier parsing...');
+
+		pdfToText = child_process.spawn('pdftotext', [X86_REF_PDF_FILE_PATH]);
+		pdfToText.on('close', function (exitCode) {
+			if (exitCode !== 0) {
+				return callback(exitCode);
+			} else {
+				return callback();
+			}
+		});
+	},
+	function openDatabase(callback) {
+		console.log('Opening database...');
+		db = new sqlite3.Database('x86-ref.sqlite', callback);
+	},
+	function dropTable(callback) {
+		console.log('Dropping existing instructions table...');
+		db.run(DROP_INSTRUCTIONS_TABLE, callback);
+	},
+	function createTable(callback) {
+		console.log('Creating new instructions table...');
+		db.run('CREATE TABLE instructions( ' 	+
+			   'mnemonic TEXT PRIMARY KEY, ' 	+
+			   'opcode TEXT, ' 					+
+			   'synopsis TEXT, ' 				+
+			   'short_description TEXT, ' 		+
+			   'long_description TEXT, ' 		+
+			   'affected_flags TEXT)', callback);
+	},
+	function parseX86RefDoc(callback) {
+		console.log('Parsing x86 reference documentation...');
+		intelX86RefParser.parse('x86.txt', function(err, instructions) {
+			return callback(err, instructions);
+		});
+	},
+	function insertData(instructions, callback) {
+		console.log('Inserting new data...');
+
+		var addInstructionStatement = db.prepare('INSERT INTO instructions VALUES (?, ?, ?, ?, ?, ?)');
+		var opcode, synopsis, longDescription, shortDescription,
+			affectedFlags;
+		for (mnemonic in instructions) {
+			opcode           = instructions[mnemonic].opcode || '';
+			synopsis		 = instructions[mnemonic].synopsis;
+			longDescription  = instructions[mnemonic].description;
+
+			if (longDescription) {
+				shortDescription = longDescription.split('.')[0] + '.';
+			}
+
+			affectedFlags    = instructions[mnemonic].affectedFlags;
+
+			addInstructionStatement.run(mnemonic,
+										opcode,
+										synopsis,
+										shortDescription,
+										longDescription,
+										affectedFlags);
+		}
+
+		addInstructionStatement.finalize(callback);
+	},
+	function cleanup(callback) {
+		var rm;
+
+		rm = child_process.spawn('rm', ['-f', X86_REF_TXT_FILE_PATH]);
+		rm.on('close', function (exitCode) {
+			callback(exitCode === 0 ? null : exitCode);
+		});
+	}], function (err, results) {
+		if (!err) {
+			console.log('Import done!');
+		} else {
+			console.log('An error occured when importing: %s', err);
+		}
 	});
-	addInstructionStatement.finalize();
-
-	db.each("SELECT mnemonic FROM instructions", function(err, row) {
-		console.log("mnemonic: " + row.mnemonic);
-	});
-});
--- tools/x86-ref-importer/intel-x86-ref-parser/index.js
+++ tools/x86-ref-importer/intel-x86-ref-parser/index.js
@@ -1,66 +1,88 @@
-var instructions = [
-	{
-		mnemonic: "AAA",
-		opcode: "37",
-		synopsis: "ASCII Adjust After Addition",
-		short_description: "Adjusts the sum of two unpacked BCD values to create an unpacked BCD result.",
-		long_description: "A long description",
-		affected_flags: "AF, CF"
-	},
-	{
-		mnemonic: "AAD",
-		opcode: "D5",
-		synopsis: "SCII Adjust AX Before Division",
-		short_description: "Adjusts two unpacked BCD digits (the least-significant digit in the AL register and the most-significantdigit in the AH register) so that adivision operation performed on the result will yield a correct unpacked BCD value. ",
-		long_description: "A long description",
-		affected_flags: "AF, CF"
-	},
-	{
-		mnemonic: "AAM",
-		opcode: "D4",
-		synopsis: "ASCII Adjust AX After Multiply",
-		short_description: "Adjusts the result of the multiplication of two unpacked BCD values to create a pair of unpacked (base10) BCD values.",
-		long_description: "A long description",
-		affected_flags: "AF, CF"
-	},
-	{
-		mnemonic: "ADD",
-		opcode: "04",
-		synopsis: "Add",
-		short_description: "Adds the destination operand (first operand) and the source operand (second operand) and then stores the result in the destination operand.",
-		long_description: "A long description",
-		affected_flags: "AF, CF"
-	},
-	{
-		mnemonic: "MOV",
-		opcode: "88",
-		synopsis: "Moves data from the second operand to the first operand.",
-		short_description: "Copies the second operand (source operand) to the first operand (destination operand).",
-		long_description: "A long description",
-		affected_flags: "AF, CF"
-	},
-	{
-		mnemonic: "CALL",
-		opcode: "E8",
-		synopsis: "Calls a procedure.",
-		short_description: "Saves procedure linking information on the stack and branches to the called procedure specified using the target operand.",
-		long_description: "A long description",
-		affected_flags: "AF, CF"
-	},
-	{
-		mnemonic: "RET",
-		opcode: "C3",
-		synopsis: "Returns from a procedure call.",
-		short_description: "Transfers program control to a return address located on th e top of the stack.",
-		long_description: "A long description",
-		affected_flags: "AF, CF"
-	}
-];
+var LineStream = require('line-stream');
+var fs         = require('fs');
+var debug	   = require('debug')('x86-ref-parser');
+
+var instructions       = {};
+
+var INSTRUCTION_HEADER_MARKER = /^[^\s]+\u0097[^\s]+/;
+var INSTRUCTION_HEADER_SEPARATOR = '\u0097';
+
+var DESCRIPTION_HEADER = /^Description$/;
+var FLAGS_AFFECTED_HEADER = /^Flags Affected$/;
+
+function parse(filePath, callback) {
+	var fileStream = fs.createReadStream(filePath);
+	var lineStream = new LineStream();
+	var mnemonicAndSynopsis = null;
+	var menmonic, synopsis;
+	var chunk;
+	var lineStr = null;
+
+	var parsingDescription    = false;
+	var parsingNewInstruction = false;
+	var parsingFlagsAffected  = false;
+
+	var currentInstructionMnemonic      = null;
+	var currentInstructionSynopsis      = null;
+	var currentInstructionDescription   = '';
+	var currentInstructionFlagsAffected = '';
+
+	lineStream.on('data', function (chunk) {
+		line = chunk.toString();
+		if (parsingNewInstruction) {
+			if (parsingDescription) {
+				if (line.length === 0) {
+					instructions[currentInstructionMnemonic].description = currentInstructionDescription;
+					parsingDescription = false;
+				} else {
+					currentInstructionDescription += ' ' + line;
+				}
+			} else if (parsingFlagsAffected) {
+				if (line.length === 0) {
+					instructions[currentInstructionMnemonic].affectedFlags = currentInstructionFlagsAffected;
+					parsingFlagsAffected = false;
+				} else {
+					currentInstructionFlagsAffected += '' + line;
+				}
+			} else if (DESCRIPTION_HEADER.test(line)) {
+				parsingDescription = true;
+				currentInstructionDescription = '';
+			} else if (FLAGS_AFFECTED_HEADER.test(line)) {
+				parsingFlagsAffected = true;
+				currentInstructionFlagsAffected = '';
+			} else if (INSTRUCTION_HEADER_MARKER.test(line)) {
+				parsingNewInstruction = false;
+				lineStream.unshift(chunk);
+			}
+		} else {
+			if (INSTRUCTION_HEADER_MARKER.test(line)) {
+				mnemonicAndSynopsis = line.split(INSTRUCTION_HEADER_SEPARATOR);
+				currentInstructionMnemonic = mnemonicAndSynopsis[0];
+				currentInstructionSynopsis = mnemonicAndSynopsis[1];
+
+				if (!instructions[currentInstructionMnemonic]) {
+					instructions[currentInstructionMnemonic] = { 'synopsis' : currentInstructionSynopsis }
+					debug("Mnemonic: %s, synopsis: %s",
+						  currentInstructionMnemonic,
+						  currentInstructionSynopsis);
+					parsingNewInstruction = true;
+				}
+
+			}
+		}
+	});
+
+	lineStream.on('end', function () {
+		callback(null, instructions);
+	});
+
+	lineStream.on('error', function (err) {
+		callback(err);
+	})
+
+	fileStream.pipe(lineStream)
+}
 
 module.exports = {
-	parse: function parse(filePath, callback) {
-		instructions.forEach(function(instruction) {
-			callback(instruction);
-		});
-	}
+	parse: parse
 }
